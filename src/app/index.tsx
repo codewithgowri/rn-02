52db89b5a0f732a83c085938a58f4f08f654757f
Gowri Shankar Mukkeri <gowrishankar.mukkeri@techwave.net>
2024-03-03 08:00:32 +0530

    implemented path aliases

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, View } from 'react-native';
-import DayListItem from '../../src/components/core/DayListItem';
+import DayListItem from '@components/core/DayListItem';
 
 
 export default function HomePage() {
